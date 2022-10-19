fe0e2dfb7c3435da0d19ddae9f16d21d65c4c557
Imache <miktor4925@gmail.com>
2022-10-19 04:50:57 +0300

    Add CopyAll method to Copier class

--- Copier.cs
+++ Copier.cs
@@ -26,6 +26,34 @@ class Copier
                 //overwriting the destination file if exists
                 File.Copy(file, dest, true); 
             }
+        }   
+        else
+        {
+            System.Console.WriteLine("Source path does not exist");
+        }
+    }
+
+    public void CopyAll(string fromPath, string toPath)
+    {
+        if(Directory.Exists(fromPath))
+        {
+            if(!Directory.Exists(toPath))
+            {
+                System.Console.WriteLine("Destination path does not exist!");
+                return;
+            }
+
+            CopyFiles(fromPath, toPath);
+            var nestedDirs = Directory.GetDirectories(fromPath);
+            
+            foreach (var dir in nestedDirs)
+            {
+                var dirName = new DirectoryInfo(dir).Name;
+                var destDir = Path.Combine(toPath, dirName);
+                Directory.CreateDirectory(destDir);
+                CopyAll(dir, destDir);    
+            }
+
         }
         else
         {
