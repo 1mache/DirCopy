6bedfaf5437f6fd6b9b2d4ec41a420273d91bfed
Imache <miktor4925@gmail.com>
2022-10-19 04:50:57 +0300

    Add CopyAll method to Copier class

--- Program.cs
+++ Program.cs
@@ -4,6 +4,6 @@ class Program
     static void Main(string[] args)
     {
         var cp = new Copier();
-        cp.CopyFiles("/home/imache/CSProjects/DirCopy/testFrom", "/home/imache/CSProjects/DirCopy/testTo");
+        cp.CopyAll("/home/imache/CSProjects/DirCopy/testFrom", "/home/imache/CSProjects/DirCopy/testTo");
     }
 }
