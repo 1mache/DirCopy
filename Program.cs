abe9904252a092c18231937ad5b20441cb1c75de
Imache <miktor4925@gmail.com>
2022-10-19 12:16:51 +0300

    Move copy methods to program class since why the hell did I put them in a Copier class

--- Program.cs
+++ Program.cs
@@ -1,9 +1,65 @@
 ﻿
 class Program
 {
+    static void CopyFiles(string fromPath, string toPath)
+    {
+        if(Directory.Exists(fromPath))
+        {
+            if(!Directory.Exists(toPath))
+            {
+                System.Console.WriteLine("Destination path does not exist!");
+                return;
+            }
+
+            //full paths of files
+            string[] files = Directory.GetFiles(fromPath);
+
+            foreach (var file in files)
+            {
+                var fileName = Path.GetFileName(file);
+                var dest = Path.Combine(toPath, fileName);
+                //overwriting the destination file if exists
+                File.Copy(file, dest, true); 
+            }
+        }   
+        else
+        {
+            System.Console.WriteLine("Source path does not exist");
+        }
+    }
+
+    static void CopyAll(string fromPath, string toPath)
+    {
+        if(Directory.Exists(fromPath))
+        {
+            if(!Directory.Exists(toPath))
+            {
+                System.Console.WriteLine("Destination path does not exist!");
+                return;
+            }
+
+            //copy all the files inside this dir
+            CopyFiles(fromPath, toPath);
+            //full paths of nested folders 
+            var nestedDirs = Directory.GetDirectories(fromPath);
+            
+            foreach (var dir in nestedDirs)
+            {
+                //gets the folder name
+                var dirName = new DirectoryInfo(dir).Name;
+                var destDir = Path.Combine(toPath, dirName);
+                Directory.CreateDirectory(destDir);
+                CopyAll(dir, destDir);    
+            }
+
+        }
+        else
+        {
+            System.Console.WriteLine("Source path does not exist");
+        }
+    }
     static void Main(string[] args)
     {
-        var cp = new Copier();
-        cp.CopyAll("/home/imache/CSProjects/DirCopy/testFrom", "/home/imache/CSProjects/DirCopy/testTo");
+        CopyAll("/home/imache/CSProjects/DirCopy/testFrom", "/home/imache/CSProjects/DirCopy/testTo");
     }
 }
