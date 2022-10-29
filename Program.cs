a7135b025d767e935a9d124e2d8b75cea0e7b6f4
Imache <miktor4925@gmail.com>
2022-10-29 17:22:55 +0300

    Create namespace

--- Program.cs
+++ Program.cs
@@ -1,89 +1,91 @@
-﻿using ConsoleOptions;
-class Program
+﻿namespace DirCopy
 {
-    static string Squiggle(string path)
+    class Program
     {
-        var curr = Directory.GetCurrentDirectory();
-        //replaces the "~/" with path of current directory 
-        return Path.Combine(curr, path.Substring(2));
-    }
-    
-    static void CopyFiles(string fromPath, string toPath)
-    {
-        if(fromPath.Substring(0,2).Equals("~/"))
+        static string Squiggle(string path)
         {
-            fromPath = Squiggle(fromPath);
-        }        
-        if(fromPath.Substring(0,2).Equals("~/"))
-        {
-            toPath = Squiggle(toPath);
+            var curr = Directory.GetCurrentDirectory();
+            //replaces the "~/" with path of current directory 
+            return Path.Combine(curr, path.Substring(2));
         }
         
-        if(Directory.Exists(fromPath))
+        static void CopyFiles(string fromPath, string toPath)
         {
-            if(!Directory.Exists(toPath))
+            if(fromPath.Substring(0,2).Equals("~/"))
             {
-                System.Console.WriteLine("Destination path does not exist!");
-                return;
-            }
-
-            //full paths of files
-            string[] files = Directory.GetFiles(fromPath);
-
-            foreach (var file in files)
+                fromPath = Squiggle(fromPath);
+            }        
+            if(fromPath.Substring(0,2).Equals("~/"))
             {
-                var fileName = Path.GetFileName(file);
-                var dest = Path.Combine(toPath, fileName);
-                //overwriting the destination file if exists
-                File.Copy(file, dest, true); 
+                toPath = Squiggle(toPath);
             }
-        }   
-        else
-        {
-            System.Console.WriteLine("Source path does not exist");
-        }
-    }
-
-    static void CopyAll(string fromPath, string toPath)
-    {
-        if(fromPath[0].Equals('~'))
-        {
-            fromPath = Squiggle(fromPath);
-        }        
-        if(toPath[0].Equals('~'))
-        {
-            toPath = Squiggle(toPath);
-        }
-
-        if(Directory.Exists(fromPath))
-        {
-            if(!Directory.Exists(toPath))
-            {
-                System.Console.WriteLine("Destination path does not exist!");
-                return;
-            }
-
-            //copy all the files inside this dir
-            CopyFiles(fromPath, toPath);
-            //full paths of nested folders 
-            var nestedDirs = Directory.GetDirectories(fromPath);
             
-            foreach (var dir in nestedDirs)
+            if(Directory.Exists(fromPath))
             {
-                //gets the folder name
-                var dirName = new DirectoryInfo(dir).Name;
-                var destDir = Path.Combine(toPath, dirName);
-                Directory.CreateDirectory(destDir);
-                CopyAll(dir, destDir);    
+                if(!Directory.Exists(toPath))
+                {
+                    System.Console.WriteLine("Destination path does not exist!");
+                    return;
+                }
+
+                //full paths of files
+                string[] files = Directory.GetFiles(fromPath);
+
+                foreach (var file in files)
+                {
+                    var fileName = Path.GetFileName(file);
+                    var dest = Path.Combine(toPath, fileName);
+                    //overwriting the destination file if exists
+                    File.Copy(file, dest, true); 
+                }
+            }   
+            else
+            {
+                System.Console.WriteLine("Source path does not exist");
+            }
+        }
+
+        static void CopyAll(string fromPath, string toPath)
+        {
+            if(fromPath[0].Equals('~'))
+            {
+                fromPath = Squiggle(fromPath);
+            }        
+            if(toPath[0].Equals('~'))
+            {
+                toPath = Squiggle(toPath);
             }
 
+            if(Directory.Exists(fromPath))
+            {
+                if(!Directory.Exists(toPath))
+                {
+                    System.Console.WriteLine("Destination path does not exist!");
+                    return;
+                }
+
+                //copy all the files inside this dir
+                CopyFiles(fromPath, toPath);
+                //full paths of nested folders 
+                var nestedDirs = Directory.GetDirectories(fromPath);
+                
+                foreach (var dir in nestedDirs)
+                {
+                    //gets the folder name
+                    var dirName = new DirectoryInfo(dir).Name;
+                    var destDir = Path.Combine(toPath, dirName);
+                    Directory.CreateDirectory(destDir);
+                    CopyAll(dir, destDir);    
+                }
+
+            }
+            else
+            {
+                System.Console.WriteLine("Source path does not exist");
+            }
         }
-        else
+        static void Main(string[] args)
         {
-            System.Console.WriteLine("Source path does not exist");
         }
-    }
-    static void Main(string[] args)
-    {
     }
 }
